1eeb4cdff2fa4762d997498da17f748cdbfda7cd
kingmaulana <akbarquipper68@gmail.com>
2025-07-28 11:03:03 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { db } from './dbConnect.js'
+import express from 'express'
+
+const app = express()
+const PORT = 8000
+
+app.use('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
+})
+
+app.use(/(.*)/, (req, res) => {
+  res.status(404).json({ message: 'not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running ons http://localhost:${PORT}`);
+});
+
+
